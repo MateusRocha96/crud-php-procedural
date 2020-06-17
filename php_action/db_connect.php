54c87e575ff5c4b5ffbd40d74f51ce39c8765155
Mateus Soares da Rocha <rocha.mateus96@gmail.com>
2020-06-17 16:56:06 -0300

    Crud basico

---
+++ php_action/db_connect.php
@@ -0,0 +1,15 @@
+<?php
+//dados de conexão
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db_name = "crud";
+
+$connect = mysqli_connect($servername, $username, $password, $db_name, "3308");
+mysqli_set_charset($connect, "utf8");
+
+if(mysqli_connect_error()):
+	echo "Erro na conexão: ".mysqli_connect_error();
+endif;
+?>
